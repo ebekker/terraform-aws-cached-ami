74a5eb7a56c997dced22ee10ef92409578b3e99f
Eugene Bekker <ebekker@users.noreply.github.com>
2018-03-13 17:51:58 -0400

    Cleaning up docs

--- variables.tf
+++ variables.tf
@@ -6,16 +6,7 @@ variable "most_recent" {
 
 variable "executable_users" {
   default = []
-  description = "See `aws_ami.most_recent`."
-}
-
-variable "filters" {
-  default = []
-  description = <<EOF
-See `aws_ami.filter`.
-Instead of specifying multiple filter sub-clauses, due to limitations on module input value
-types, these have to be specified as an array of objects with `name` and `values` properties.
-EOF
+  description = "See `aws_ami.executable_users`."
 }
 
 variable "owners" {
@@ -28,16 +19,33 @@ variable "name_regex" {
   description = "See `aws_ami.name_regex`."
 }
 
+variable "filters" {
+  default = []
+  description = <<EOF
+See `aws_ami.filter`.
+Instead of specifying multiple filter sub-clauses, due to limitations on module input value
+types, these have to be specified as an array of objects with `name` and `values` properties.
+EOF
+}
+
+
 variable "cache_key" {
-  description = "A unique key across the named cache that uniquely identifies this module's resolve value"
+  description = <<EOF
+A unique key across the named cache root that uniquely identifies this module instance's resolved values.
+EOF
 }
 
 variable "cache_expires" {
   default = -1
-  description = "Time in seconds of how long to cache a value."
+  description = <<EOF
+Time in seconds of how long to cache a value.
+If unspecifed, defaults to cache expires setting of the supporting tools (currently 12 hours).
+EOF
 }
 
 variable "cache_root" {
   default = "_TMP/amis"
-  description = "The path of a cache root directory which will store the cached value."
+  description = <<EOF
+The path of a cache root directory which will store the cached value.
+EOF
 }
